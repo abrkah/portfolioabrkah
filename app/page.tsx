05c42d1fdfc5f66ca34fc34ee1222b7b5de89ab6
abrkah <abirha182@gmail.com>
2025-01-13 11:38:02 +0300

    add

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,13 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero />
+        {/* <Hero /> */}
         {/* <Grid /> */}
-        {/* <RecentProjects />
-        <Clients />
+        <RecentProjects />
+        {/* <Clients />
         <Experience />
         <Approach />
-        <Footer /> */}
+        <Footer />  */}
       </div>
     </main>
   );
